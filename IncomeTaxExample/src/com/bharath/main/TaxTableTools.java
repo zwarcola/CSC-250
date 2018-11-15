b7c6fe1bf55770215ceafada310e52b99bcd4244
zwarcola <42807498+zwarcola@users.noreply.github.com>
2018-11-15 15:05:29 -0500

    Update TaxTableTools.java

--- IncomeTaxExample/src/com/bharath/main/TaxTableTools.java
+++ IncomeTaxExample/src/com/bharath/main/TaxTableTools.java
@@ -4,7 +4,7 @@ public class TaxTableTools {
 	/** This class searches the 'search' table with a search argument and
 		returns the corresponding value in the 'value' table. Variable
 		'nEntries' has the number of entries in each table.
-     */
+         */
 	private int[] search =   {   0,  20000, 50000, 100000, Integer.MAX_VALUE };
     private double[] value = { 0.0,   0.10,  0.20,   0.30, 0.40 };
     private int nEntries;
